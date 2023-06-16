aa1a5166c7c9c241b8931211735cd20fd479aa7a
herokudev <ao43085@gmail.com>
2023-06-16 16:47:20 -0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import "./App.css";
+import photo1 from "./assets/Hiking4.jpg";
 
 function App() {
   return (
     <>
-      <div className="cover">
-        <h1>Discover what's out there.</h1>
-        <form className="flex-form">
-          <label htmlFor="from">
-            <i className="ion-location" />
-          </label>
-          <input type="search" placeholder="Where do you want to go?" />
-          <input type="submit" defaultValue="Search" />
-        </form>
-        <div id="madeby">
-          <span>
-            Photo by{" "}
-            <a href="https://unsplash.com/@benblenner" target="_blank">
-              Ben Blennerhassett
-            </a>
-          </span>
-        </div>
+      <div className='cover'>
+        <img src={photo1} alt='img-hiking' />
       </div>
     </>
   );
